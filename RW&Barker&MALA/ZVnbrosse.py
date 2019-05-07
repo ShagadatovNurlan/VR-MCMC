861c9e7790106b5b6792fa8eafe3449b134fa35d
Nurlan Shagadatov <nurlan.shagadatov@gmail.com>
2019-05-07 23:13:53 +0300

    added mala initial tests

---
+++ RW&Barker&MALA/ZVnbrosse.py
@@ -0,0 +1,110 @@
+import numpy as np
+
+def ZVpolyOne(traj, traj_grad, f_target= "sum"):
+    n, d = traj.shape
+    if f_target == "sum":
+        samples = traj.sum(axis = 1).reshape(-1,1)
+    elif f_target == "sum_squared":
+        samples = np.square(traj).sum(axis = 1).reshape(-1,1)
+    elif f_target == "sum_4th":
+        samples = ((traj)**4).sum(axis = 1).reshape(-1,1)
+    elif f_target == "exp_sum":
+        samples = np.exp(traj.sum(axis = 1)).reshape(-1,1)
+    else:
+        raise Exception('unrecognized target function')
+
+    cov1 = np.cov(traj_grad, rowvar=False)
+    A = np.linalg.inv(cov1)
+    covariance = np.cov(np.concatenate((-traj_grad, samples), axis=1), rowvar=False)
+    paramZV1 = -np.dot(A,covariance[:d, d:])
+    ZV1 = samples - np.dot(traj_grad, paramZV1)
+    mean_ZV1 = np.mean(ZV1, axis = 0)
+    return mean_ZV1
+
+def ZVpolyTwo(traj, traj_grad, f_target):
+    n, d = traj.shape
+    if f_target == "sum":
+        samples = traj.sum(axis = 1).reshape(-1,1)
+    elif f_target == "sum_squared":
+        samples = np.square(traj).sum(axis = 1).reshape(-1,1)
+    elif f_target == "sum_4th":
+        samples = ((traj)**4).sum(axis = 1).reshape(-1,1)
+    elif f_target == "exp_sum":
+        samples = np.exp(traj.sum(axis = 1)).reshape(-1,1)
+    else:
+        raise Exception('unrecognized target function')
+    Lpoisson = np.zeros((n,int(d*(d+3)/2)))
+    Lpoisson[:,np.arange(d)] = - traj_grad
+    Lpoisson[:,np.arange(d, 2*d)] = 2*(1. - np.multiply(traj, traj_grad))
+    k=2*d
+    for j in np.arange(d-1):
+        for i in np.arange(j+1,d):
+            Lpoisson[:,k] = -np.multiply(traj_grad[:,i], traj[:,j]) \
+                    -np.multiply(traj_grad[:,j], traj[:,i])
+            k=k+1
+    
+    cov1 = np.cov(Lpoisson, rowvar=False)
+    A = np.linalg.inv(cov1)
+    cov2 = np.cov(np.concatenate((Lpoisson, samples),axis=1), rowvar=False)
+    B = cov2[0:int(d*(d+3)/2), int(d*(d+3)/2):]
+    paramZV2 = - np.dot(A,B)
+    ZV2 = samples + np.dot(Lpoisson, paramZV2)
+    mean_ZV2 = np.mean(ZV2, axis = 0)
+    return mean_ZV2
+
+def CVpolyOne(traj,traj_grad, f_target):
+    n, d = traj.shape
+    if f_target == "sum":
+        samples = traj.sum(axis = 1).reshape(-1,1)
+    elif f_target == "sum_squared":
+        samples = np.square(traj).sum(axis = 1).reshape(-1,1)
+    elif f_target == "sum_4th":
+        samples = ((traj)**4).sum(axis = 1).reshape(-1,1)
+    elif f_target == "exp_sum":
+        samples = np.exp(traj.sum(axis = 1)).reshape(-1,1)
+    else:
+        raise Exception('unrecognized target function')
+    covariance = np.cov(np.concatenate((traj, samples), axis=1), rowvar=False)
+    paramCV1 = covariance[:d, d:]
+    CV1 = samples - np.dot(traj_grad, paramCV1)
+    mean_CV1 = np.mean(CV1, axis = 0)
+    return mean_CV1
+
+def CVpolyTwo(traj, traj_grad, f_target):
+    n, d = traj.shape
+    if f_target == "sum":
+        samples = traj.sum(axis = 1).reshape(-1,1)
+    elif f_target == "sum_squared":
+        samples = np.square(traj).sum(axis = 1).reshape(-1,1)
+    elif f_target == "sum_4th":
+        samples = ((traj)**4).sum(axis = 1).reshape(-1,1)
+    elif f_target == "exp_sum":
+        samples = np.exp(traj.sum(axis = 1)).reshape(-1,1)
+    else:
+        raise Exception('unrecognized target function')
+    poisson = np.zeros((n,int(d*(d+3)/2)))
+    poisson[:,np.arange(d)] = traj
+    poisson[:,np.arange(d, 2*d)] = np.multiply(traj, traj)
+    k = 2*d
+    for j in np.arange(d-1):
+        for i in np.arange(j+1,d):
+            poisson[:,k] = np.multiply(traj[:,i], traj[:,j])
+            k=k+1
+    Lpoisson = np.zeros((n,int(d*(d+3)/2)))
+    Lpoisson[:,np.arange(d)] = - traj_grad
+    Lpoisson[:,np.arange(d, 2*d)] = 2*(1. - np.multiply(traj, traj_grad))
+    k=2*d
+    for j in np.arange(d-1):
+        for i in np.arange(j+1,d):
+            Lpoisson[:,k] = -np.multiply(traj_grad[:,i], traj[:,j]) \
+                    -np.multiply(traj_grad[:,j], traj[:,i])
+            k=k+1
+    
+    cov1 = np.cov(np.concatenate((poisson, -Lpoisson), axis=1), rowvar=False)
+    A = np.linalg.inv(cov1[0:int(d*(d+3)/2), int(d*(d+3)/2):d*(d+3)])
+    cov2 = np.cov(np.concatenate((poisson, samples),axis=1), rowvar=False)
+    B = cov2[0:int(d*(d+3)/2), int(d*(d+3)/2):]
+    paramCV2 = np.dot(A,B)
+    CV2 = samples + np.dot(Lpoisson, paramCV2)
+    mean_CV2 = np.mean(CV2, axis = 0)
+    return mean_CV2
